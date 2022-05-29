413620948de974ff3979e74f96ebea1c4b910bc3
zare2k <101191719+zare2k@users.noreply.github.com>
2022-05-29 20:23:50 +0200

    povezivanje sa klijentom

---
+++ replicator_sender.py
@@ -0,0 +1,13 @@
+import socket     
+
+def konekcija():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((socket.gethostname(), 8081))
+    s.listen(1)
+    print("Cekam konekciju...")
+    soket, adresa = s.accept()
+    print("Konektovan klijent sa adrese: ", adresa)
+    return soket
+
+if __name__ == "__main__":
+    soket = konekcija()
