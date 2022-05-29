af995c63b61372617b4d42d4a621874b334485f0
Sevatrox <101191688+Sevatrox@users.noreply.github.com>
2022-05-29 20:50:47 +0200

    preuzimanje podataka i slanje replikatoru

--- writer.py
+++ writer.py
@@ -1,18 +1,31 @@
 import socket
 import podatak
+import pickle
 
 def konekcija():
     klijent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     klijent.connect((socket.gethostname(), 8081))
     return klijent
 
+def slanje(klijent, id, potrosnja):
+    podaci = pickle.dumps(podatak.Podatak(id, potrosnja))
+    klijent.send(podaci)
+
 if __name__ == "__main__":
 
-    #klijent = konekcija()
+    klijent = konekcija()
 
     while True: 
 
         odgovor = input("Meni: \n1 - Unos potrosnje vode\n2 - Izlaz\n")
 
-        if int(odgovor) == 2:
-            break        
+        if int(odgovor) == 1:
+            id = input("Unesite ID brojila: ")
+            potrosnja = input("Unesite potrosnju vode: ")
+            slanje(klijent, id, potrosnja)
+
+        elif int(odgovor) == 2:
+            klijent.close()
+            break
+        else:
+            print("Niste izabrali validnu opciju!\n")
