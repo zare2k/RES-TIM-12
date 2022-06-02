5b01ac019368c3ceea51a31b9cd69a1d61b11ff3
GajicAleksandra <101862217+GajicAleksandra@users.noreply.github.com>
2022-06-02 22:47:10 +0200

    Update podatak.py

--- podatak.py
+++ podatak.py
@@ -1,4 +1,5 @@
 class Podatak:
-    def __init__(self, idBrojila, potrosnjaVode):
-        self.idBrojila = idBrojila
-        self.potrosnjaVode = potrosnjaVode
+    def __init__(self, id_brojila, potrosnja_vode, redni_broj=None):
+        self.id_brojila = id_brojila
+        self.potrosnja_vode = potrosnja_vode
+        self.redni_broj = redni_broj
