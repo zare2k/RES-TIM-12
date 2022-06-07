6e366e6da824b83b4db68e08981bc8d3907c5339
Mašan Bulajić <101191685+Masan04@users.noreply.github.com>
2022-06-07 19:37:31 +0200

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -4,7 +4,7 @@ baza = database_functions.konekcija()
 myCursor = baza.cursor()
 
 myCursor.execute("CREATE TABLE BROJILO(ID int PRIMARY KEY NOT NULL AUTO_INCREMENT, IME VARCHAR(50) NOT NULL, PREZIME VARCHAR(50) NOT NULL, ULICA VARCHAR(50) NOT NULL, BROJ_ULICE int NOT NULL, POSTANSKI_BROJ int NOT NULL, GRAD VARCHAR(50) NOT NULL)")
-myCursor.execute("CREATE TABLE POTROSNJA_BROJILA(REDNI_BROJ int PRIMARY KEY NOT NULL AUTO_INCREMENT, ID int NOT NULL, POSTROSNJA int NOT NULL, MESEC VARCHAR(50) NOT NULL)")
+myCursor.execute("CREATE TABLE POTROSNJA_BROJILA(REDNI_BROJ int PRIMARY KEY NOT NULL AUTO_INCREMENT, ID int NOT NULL, POTROSNJA int NOT NULL, MESEC VARCHAR(50) NOT NULL)")
   
 myCursor.execute("INSERT INTO brojilo(id, ime, prezime, ulica, broj_ulice, postanski_broj, grad) VALUES ('1', 'Marko', 'Markovic', 'Sindjeliceva', '45', '15000', 'Sabac')")
 myCursor.execute("INSERT INTO brojilo (id, ime, prezime, ulica, broj_ulice, postanski_broj, grad) VALUES ('2', 'Petar', 'Petrovic', 'Kosovska', '105', '21000', 'Novi Sad')")
