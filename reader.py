779c7af8e49d88225a6b2c49dfcdf33eb222d8b4
Sevatrox <101191688+Sevatrox@users.noreply.github.com>
2022-06-07 22:01:04 +0200

    pisanje exception-a za reader komponentu

--- reader.py
+++ reader.py
@@ -1,6 +1,7 @@
 import socket
 import pickle
-import podatak
+import database_functions
+import random
 
 def konekcija():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,11 +14,33 @@ def konekcija():
 
 if __name__ == "__main__":
     
-    soket = konekcija()
+    try:
+        soket = konekcija()
+    except socket.error:
+        print("Greska u konekciji sa replikatorom.")
+        exit(1)
+
+    baza = database_functions.konekcija()
 
     while True:
-        podaci = pickle.loads(soket.recv(4096))
+        try:
+            podaci = pickle.loads(soket.recv(4096))
+        except EOFError:
+            odgovor = input("Ugasen klijent, da li zelite da ugasite server? (DA/NE)")
+            if odgovor == "NE":
+                soket = konekcija()
+                continue
+            elif odgovor == "DA":
+                soket.close()
+                break
+            else:
+                print("Unesite DA ili NE")
+                continue
 
         print("Podaci stigli od klijenta: ")
         print("ID brojila: ", podaci.id_brojila)
         print("Potrosnja vode: ", podaci.potrosnja_vode)
+        
+        niz = ['Januar', 'Februar', 'Mart', 'April', 'Maj', 'Jun', 'Jul', 'Avgust', 'Septembar', 'Oktobar', 'Novembar', 'Decembar']
+        mesec = random.choice(niz)
+        database_functions.dodaj_element(podaci.id_brojila, podaci.potrosnja_vode, mesec, baza)
