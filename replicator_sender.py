809c76aaacefa6db669132ebc932fa677f724147
Mašan Bulajić <101191685+Masan04@users.noreply.github.com>
2022-06-09 23:06:04 +0200

    Update replicator_sender.py

--- replicator_sender.py
+++ replicator_sender.py
@@ -4,15 +4,22 @@ from _thread import *
 
 def visestruka_konekcija(konekcija, klijent):
     while True:
-        data = konekcija.recv(4096)
-        if not data:
+        try:
+            data = konekcija.recv(4096)
+            if not data:
+                break
+            podaci = pickle.loads(data)
+        except ConnectionResetError:
             break
-        podaci = pickle.loads(data)
+        
         print("Podaci stigli od klijenta: ")
         print("ID brojila: ", podaci.id_brojila)
         print("Potrosnja vode: ", podaci.potrosnja_vode)
         
-        slanje_receiver(klijent, podaci)
+        try:
+            slanje_receiver(klijent, podaci)
+        except socket.error:
+            break
     konekcija.close()
 
 def konekcija():
@@ -33,12 +40,20 @@ def slanje_receiver(klijent, podaci):
 
 if __name__ == "__main__":
     
-    s = konekcija()
-    klijent = konekcija_receiver()
-    
+    try:
+        s = konekcija()
+    except socket.error:
+        print("Neuspesna konekcija sa writer-om.")
+        exit(1)
+        
+    try:
+        klijent = konekcija_receiver()
+    except socket.error:
+        print("Neuspesna konekcija sa replicator receiver-om")
+        exit(1)
+        
     while True:
         soket, adresa = s.accept()
-        print("Konektovan klijent sa adrese: ", adresa)
-        
+        print("Konektovan klijent sa adrese: ", adresa) 
+
         start_new_thread(visestruka_konekcija, (soket, klijent))
-        
