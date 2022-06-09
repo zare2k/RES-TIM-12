0b2c1c200eaaee2c7fe54b3f0a30773f6acfc9af
zare2k <101191719+zare2k@users.noreply.github.com>
2022-06-09 18:19:28 +0200

    Update replicator_receiver.py

--- replicator_receiver.py
+++ replicator_receiver.py
@@ -16,26 +16,50 @@ def konekcija_reader():
     receiver.connect((socket.gethostname(), 8083))
     return receiver
 
-def slanje_reader(receiver, id, potrosnja):
-    podaci = pickle.dumps(podatak.Podatak(id, potrosnja))
-    receiver.send(podaci)
+def slanje_reader(receiver, podaci):
+    podaci_bytes = pickle.dumps(podaci)
+    receiver.send(podaci_bytes)
 
 if __name__ == "__main__":
     
-    soket = konekcija()
-    receiver = konekcija_reader()
+    try:
+        soket = konekcija()
+    except socket.error:
+        print("Greska u konekciji sa replikator senderom.")
+        exit(1)
+    try:
+        receiver = konekcija_reader()
+    except socket.error:
+        print("Greska u konekciji sa reader-om.")
+        exit(1)
     
     lista = list()
     redni_broj = 0
     
     while True:
-        podaci = pickle.loads(soket.recv(4096))
-        
-        lista.append((podaci, redni_broj))
-        redni_broj += 1
-        
+        try:
+            podaci = pickle.loads(soket.recv(4096))
+            podaci.redni_broj = redni_broj
+            redni_broj += 1
+            lista.append(podaci)
+        except EOFError:
+            odgovor = input("Ugasen klijent, da li zelite da ugasite server? (DA/NE)")
+            if odgovor == "NE":
+                soket = konekcija()
+                continue
+            elif odgovor == "DA":
+                soket.close()
+                break
+            else:
+                print("Unesite DA ili NE")
+                continue
+
         print("Podaci stigli od klijenta: ")
-        print("ID brojila: ", podaci.idBrojila)
-        print("Potrosnja vode: ", podaci.potrosnjaVode)
+        print("ID brojila: ", podaci.id_brojila)
+        print("Potrosnja vode: ", podaci.potrosnja_vode)
         
-        slanje_reader(receiver, podaci.idBrojila, podaci.potrosnjaVode)
+        try:
+            slanje_reader(receiver, podaci)
+        except EOFError:
+            print("Neuspesno slanje podataka reader komponenti.")
+            exit(2)
