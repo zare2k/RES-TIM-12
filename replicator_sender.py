acac07c32e195477e395bedcb330843bfc3fafd1
Mašan Bulajić <101191685+Masan04@users.noreply.github.com>
2022-06-09 23:05:02 +0200

    Update replicator_sender.py

--- replicator_sender.py
+++ replicator_sender.py
@@ -1,35 +1,60 @@
 import socket
 import pickle
-import podatak
+from _thread import *
+
+def visestruka_konekcija(konekcija, klijent):
+    while True:
+        try:
+            data = konekcija.recv(4096)
+            if not data:
+                break
+            podaci = pickle.loads(data)
+        except ConnectionResetError:
+            break
+        
+        print("Podaci stigli od klijenta: ")
+        print("ID brojila: ", podaci.id_brojila)
+        print("Potrosnja vode: ", podaci.potrosnja_vode)
+        
+        try:
+            slanje_receiver(klijent, podaci)
+        except socket.error:
+            break
+    konekcija.close()
 
 def konekcija():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((socket.gethostname(), 8081))
     s.listen(5)
     print("Cekam konekciju...")
-    soket, adresa = s.accept()
-    print("Konektovan klijent sa adrese: ", adresa)
-    return soket
+    return s
 
 def konekcija_receiver():
     klijent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     klijent.connect((socket.gethostname(), 8082))
     return klijent
     
-def slanje_receiver(klijent, id, potrosnja):
-    podaci = pickle.dumps(podatak.Podatak(id, potrosnja))
-    klijent.send(podaci)
+def slanje_receiver(klijent, podaci):
+    podaci_bytes = pickle.dumps(podaci)
+    klijent.send(podaci_bytes)
 
 if __name__ == "__main__":
     
-    soket = konekcija()
-    klijent = konekcija_receiver()
-    
-    while True:
-        podaci = pickle.loads(soket.recv(4096))
-        print("Podaci stigli od klijenta: ")
-        print("ID brojila: ", podaci.idBrojila)
-        print("Potrosnja vode: ", podaci.potrosnjaVode)
+    try:
+        s = konekcija()
+    except socket.error:
+        print("Neuspesna konekcija sa writer-om.")
+        exit(1)
         
-        slanje_receiver(klijent, podaci.idBrojila, podaci.potrosnjaVode)
+    try:
+        klijent = konekcija_receiver()
+    except socket.error:
+        print("Neuspesna konekcija sa replicator receiver-om")
+        exit(1)
+        
+    while True:
+        soket, adresa = s.accept()
+        print("Konektovan klijent sa adrese: ", adresa) 
+
+        start_new_thread(visestruka_konekcija, (soket, klijent))
         
