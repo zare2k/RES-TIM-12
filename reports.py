065deeab463b25931f0dc852fce0c378981d25b8
Mašan Bulajić <101191685+Masan04@users.noreply.github.com>
2022-06-10 19:12:03 +0200

    izračunavanje mesečne potrošnje za brojilo

--- reports.py
+++ reports.py
@@ -6,5 +6,14 @@ def izvestaj_ulica(ulica):
     lista.append(ulica)
     myCursor = baza.cursor() 
     myCursor.execute("SELECT brojilo.ulica, sum(potrosnja_brojila.potrosnja), potrosnja_brojila.mesec FROM baza_podataka.potrosnja_brojila JOIN baza_podataka.brojilo WHERE brojilo.id = potrosnja_brojila.id AND brojilo.ulica = %s GROUP BY potrosnja_brojila.mesec", lista)
-    result = myCursor.fetchall()
-    return result
+    rezultat = myCursor.fetchall()
+    return rezultat
+
+def izvestaj_brojilo(brojilo):
+    baza = database_functions.konekcija()
+    lista = list()
+    lista.append(brojilo)
+    myCursor = baza.cursor()
+    myCursor.execute("SELECT brojilo.id, sum(potrosnja_brojila.potrosnja), potrosnja_brojila.mesec FROM baza_podataka.potrosnja_brojila JOIN baza_podataka.brojilo WHERE brojilo.id = potrosnja_brojila.id AND brojilo.id = %s GROUP BY potrosnja_brojila.mesec;", lista)
+    rezultat = myCursor.fetchall()
+    return rezultat
