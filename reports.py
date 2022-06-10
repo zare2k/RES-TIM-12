f52b224b68e11e7d17c261dfbf5afc1aa01f297f
zare2k <101191719+zare2k@users.noreply.github.com>
2022-06-10 17:39:36 +0200

    izracunavanje mesecne potrosnje za ulicu

--- reports.py
+++ reports.py
@@ -1,5 +1,10 @@
 import database_functions
 
-if __name__ == "__main__":
-    
+def izvestaj_ulica(ulica):
     baza = database_functions.konekcija()
+    lista = list()
+    lista.append(ulica)
+    myCursor = baza.cursor() 
+    myCursor.execute("SELECT brojilo.ulica, sum(potrosnja_brojila.potrosnja), potrosnja_brojila.mesec FROM baza_podataka.potrosnja_brojila JOIN baza_podataka.brojilo WHERE brojilo.id = potrosnja_brojila.id AND brojilo.ulica = %s GROUP BY potrosnja_brojila.mesec", lista)
+    result = myCursor.fetchall()
+    return result
