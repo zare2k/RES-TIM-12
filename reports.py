01cd19e461b10d9f5c55fbb90b57c94a8cc215af
Mašan Bulajić <101191685+Masan04@users.noreply.github.com>
2022-06-11 20:19:37 +0200

    exceptioni za reports komponentu

--- reports.py
+++ reports.py
@@ -1,3 +1,4 @@
+import mysql.connector
 import database_functions
 
 def izvestaj_ulica(ulica):
@@ -5,8 +6,14 @@ def izvestaj_ulica(ulica):
     lista = list()
     lista.append(ulica)
     myCursor = baza.cursor() 
-    myCursor.execute("SELECT brojilo.ulica, sum(potrosnja_brojila.potrosnja), potrosnja_brojila.mesec FROM baza_podataka.potrosnja_brojila JOIN baza_podataka.brojilo WHERE brojilo.id = potrosnja_brojila.id AND brojilo.ulica = %s GROUP BY potrosnja_brojila.mesec", lista)
-    rezultat = myCursor.fetchall()
+
+    try:
+        myCursor.execute("SELECT brojilo.ulica, sum(potrosnja_brojila.potrosnja), potrosnja_brojila.mesec FROM baza_podataka.potrosnja_brojila JOIN baza_podataka.brojilo WHERE brojilo.id = potrosnja_brojila.id AND brojilo.ulica = %s GROUP BY potrosnja_brojila.mesec", lista)
+        rezultat = myCursor.fetchall()
+    except mysql.connector.Error:
+        print("Greska prilikom izvrsavanja upita u bazi.")
+        return None
+
     return rezultat
 
 def izvestaj_brojilo(brojilo):
@@ -14,6 +21,11 @@ def izvestaj_brojilo(brojilo):
     lista = list()
     lista.append(brojilo)
     myCursor = baza.cursor()
-    myCursor.execute("SELECT brojilo.id, potrosnja_brojila.potrosnja, potrosnja_brojila.mesec FROM baza_podataka.potrosnja_brojila JOIN baza_podataka.brojilo WHERE brojilo.id = potrosnja_brojila.id AND brojilo.id = %s GROUP BY potrosnja_brojila.mesec;", lista)
-    rezultat = myCursor.fetchall()
+    try:
+        myCursor.execute("SELECT brojilo.id, potrosnja_brojila.potrosnja, potrosnja_brojila.mesec FROM baza_podataka.potrosnja_brojila JOIN baza_podataka.brojilo WHERE brojilo.id = potrosnja_brojila.id AND brojilo.id = %s GROUP BY potrosnja_brojila.mesec;", lista)
+        rezultat = myCursor.fetchall()
+    except mysql.connector.Error:
+        print("Greska prilikom izvrsavanja upita u bazi.")
+        return None
+    
     return rezultat
