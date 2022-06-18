8b0e8c3adf5019e40ab12e75a956ec499269c7d6
GajicAleksandra <101862217+GajicAleksandra@users.noreply.github.com>
2022-06-18 22:16:00 +0200

    bug fixing writer komponente

--- writer.py
+++ writer.py
@@ -2,56 +2,91 @@ import socket
 import podatak
 import pickle
 
+class NevalidanUnos(Exception):
+    def __init__(self, message=None):
+        self.message = message
+        
+    def __str__(self):
+        if self.message is None:
+            return "Nevalidan unos."
+        return self.message
+
 def konekcija():
-    klijent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    klijent.connect((socket.gethostname(), 8081))
-    return klijent
-
-def slanje(klijent, id, potrosnja):
-    podaci = pickle.dumps(podatak.Podatak(id, potrosnja))
-    klijent.send(podaci)
-
-if __name__ == "__main__":
-
     try:
-        klijent = konekcija()
+        klijent = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        klijent.connect((socket.gethostname(), 8081))
+        return klijent
     except socket.error:
         print("Greska u konekciji sa replikatorom.")
-        exit(1)
-    
-    while True: 
+        exit()
 
-        odgovor = input("Meni: \n1 - Unos potrosnje vode\n2 - Izlaz\n")
+def slanje(klijent, id, potrosnja):
+    try:
+        podaci = pickle.dumps(podatak.Podatak(id, potrosnja))
+        klijent.send(podaci)
+    except socket.error:
+        print("Neuspesno slanje podataka.")
+        klijent.close()
+        exit()
+
+def slanje_exit(klijent, poruka_gasenja):
+    try:
+        podaci = pickle.dumps(poruka_gasenja)
+        klijent.send(podaci)
+    except socket.error:
+        print("Neuspesno gasenje servera.")
+        klijent.close()
+    
+def meni(odgovor):
+    
+    try:
+        odgovor = int(odgovor)
+        if odgovor <= 0 or odgovor > 2:
+            raise NevalidanUnos("Unesite 1 ili 2.")
         
-        try:
-            odgovor = int(odgovor)
-        except ValueError:
-            print("Unesite broj")
+        return odgovor
+    except NevalidanUnos as e:
+        print(e)
+        return None
+    except Exception:
+        print("Unesite broj.")
+        return None
+    
+def unos(odgovor):
+    try:
+        odgovor = int(odgovor)
+        if odgovor <= 0:
+            raise NevalidanUnos()
+        
+        return odgovor
+    except NevalidanUnos as e:
+        print(e)
+        return None
+    except Exception:
+        print("Unesite broj.")
+        return None
+    
+def main():
+
+    klijent = konekcija()
+        
+    while True:
+        print("Meni: \n1 - Unos potrosnje vode\n2 - Izlaz\n")
+        
+        odgovor = meni(input())
+            
+        if odgovor == None:
             continue
-        
         if odgovor == 1:
+            print("Unesite ID brojila: ")
+            id = unos(input())
+            print("Unesite potrosnju vode: ")
+            potrosnja = unos(input())
+            slanje(klijent, id, potrosnja)
             
-            try:
-                id = int(input("Unesite ID brojila: "))
-            except ValueError:
-                print("ID mora biti broj")
-                continue
-            
-            try:
-                potrosnja = int(input("Unesite potrosnju vode: "))
-            except ValueError:
-                print("Morate uneti broj")
-                continue
-            
-            try:
-                slanje(klijent, id, potrosnja)
-            except socket.error:
-                print("Neuspesno slanje podataka.")
-                klijent.close()
-                exit(1)
-                
         elif odgovor == 2:
-            klijent.close()
+            slanje_exit(klijent, "EXIT")
             break
-        else:
-            print("Niste izabrali validnu opciju!\n")
+            
+if __name__ == "__main__":
+    main()
